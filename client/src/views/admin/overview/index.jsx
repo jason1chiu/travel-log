453a22f7703013c89a2e5db9f390fdf2d899deab
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-07-09 00:39:36 -0400

    use evenly layouts

--- client/src/views/admin/overview/index.jsx
+++ client/src/views/admin/overview/index.jsx
@@ -2,7 +2,7 @@ import { GET_ME } from "utils/queries";
 import { useQuery } from "@apollo/client";
 
 // Chakra imports
-import { Box, Grid } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 
 // Custom components
 import Banner from "views/admin/overview/components/Banner";
@@ -29,17 +29,8 @@ export default function Overview() {
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       {/* Main Fields */}
-      <Grid
-        templateColumns={{
-          base: "1fr",
-          lg: "1fr 1fr 1fr", // This will divide the grid into 3 equal columns
-        }}
-        gap={{ base: "20px", xl: "20px" }}>
+      <SimpleGrid columns={{ base: 1, md: 3 }} spacing="20px">
         <Banner
-          gridArea={{
-            base: "1 / 1 / 2 / 2",
-            lg: "1 / 1 / 2 / 2", // This will place the Banner in the first column
-          }}
           banner={banner}
           avatar={avatar}
           name={name}
@@ -48,39 +39,9 @@ export default function Overview() {
           followers='9.7k'
           following='274'
         />
-        <General
-          gridArea={{
-            base: "2 / 1 / 3 / 2",
-            lg: "1 / 2 / 2 / 3", // This will place the General in the second column
-          }}
-          minH='365px'
-          pe='20px'
-        />
-        <Upload
-          gridArea={{
-            base: "3 / 1 / 4 / 2",
-            lg: "1 / 3 / 2 / 4", // This will place the Upload in the third column
-          }}
-          minH={{ base: "auto", lg: "420px", "2xl": "365px" }}
-          pe='20px'
-          pb={{ base: "100px", lg: "20px" }}
-        />
-      </Grid>
-      <Grid
-        mb='20px'
-        templateColumns={{
-          base: "1fr",
-          lg: "repeat(2, 1fr)",
-          "2xl": "1.34fr 1.62fr 1fr",
-        }}
-        templateRows={{
-          base: "1fr",
-          lg: "repeat(2, 1fr)",
-          "2xl": "1fr",
-        }}
-        gap={{ base: "20px", xl: "20px" }}>
+        <General minH='365px' pe='20px' />
+        <Upload minH={{ base: "auto", lg: "420px", "2xl": "365px" }} pe='20px' pb={{ base: "100px", lg: "20px" }} />
         <Projects
-          gridArea='1 / 2 / 2 / 2'
           banner={banner}
           avatar={avatar}
           name='Adela Parkson'
@@ -89,21 +50,9 @@ export default function Overview() {
           followers='9.7k'
           following='274'
         />
-        <General
-          gridArea={{ base: "2 / 1 / 3 / 2", lg: "1 / 2 / 2 / 3" }}
-          minH='365px'
-          pe='20px'
-        />
-        <Notifications
-          used={25.6}
-          total={50}
-          gridArea={{
-            base: "3 / 1 / 4 / 2",
-            lg: "2 / 1 / 3 / 3",
-            "2xl": "1 / 3 / 2 / 4",
-          }}
-        />
-      </Grid>
+        <General minH='365px' pe='20px' />
+        <Notifications used={25.6} total={50} />
+      </SimpleGrid>
     </Box>
   );
 }
--- client/src/views/admin/settings/index.jsx
+++ client/src/views/admin/settings/index.jsx
@@ -9,6 +9,8 @@ import Info from "views/admin/settings/components/Info";
 import Password from "views/admin/settings/components/Password";
 import Profile from "views/admin/settings/components/Profile";
 import Socials from "views/admin/settings/components/Socials";
+import Upload from "views/admin/settings/components/Upload";
+import Delete from "views/admin/settings/components/Delete"
 
 export default function Settings() {
 
@@ -16,13 +18,15 @@ export default function Settings() {
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <SimpleGrid
         mb='20px'
-        columns={{ base: 1, sm: 1, md: 1, lg: 4, xl: 4 }}
+        columns={{ base: 1, sm: 1, md: 3, lg: 3, xl: 3 }}
         spacing={{ base: "20px", xl: "20px" }}>
         {/* Components */}
         <Profile avatar={profile} banner={banner} />
         <Info />
-        <Socials />
         <Password />
+        <Upload />
+        <Socials />
+        <Delete />
       </SimpleGrid>
     </Box>
   );
