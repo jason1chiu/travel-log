d692b54d5d7aec2adf71e834ad48af72132cd662
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-07-10 19:53:41 -0400

    added some links with basic layout

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,19 +2,37 @@ import React from "react";
 
 import { Icon } from "@chakra-ui/react";
 
-import { MdHome, MdSettings } from "react-icons/md";
+import { MdBook, MdManageAccounts } from "react-icons/md";
+import { SiYourtraveldottv } from "react-icons/si";
+import { FaUserEdit } from "react-icons/fa";
+import { BiUserCircle } from "react-icons/bi"
 
 import Overview from "views/admin/overview";
 import SignIn from "views/auth/signIn/SignInCentered";
 import SignUp from "views/auth/signUp/SignUpCentered";
-import AccountSettings from "views/admin/settings/account/index"
+import AccountSettings from "views/admin/settings/account"
+import ProfileSettings from "views/admin/settings/profile"
 
 const routes = [
   {
-    name: "Overview",
+    name: "Profile",
     layout: "/admin",
     path: "/overview",
-    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={ BiUserCircle } width='20px' height='20px' color='inherit' />,
+    component: Overview,
+  },
+  {
+    name: "Travel Plans",
+    layout: "/admin",
+    path: "/travelplans",
+    icon: <Icon as={ SiYourtraveldottv } width='20px' height='20px' color='inherit' />,
+    component: Overview,
+  },
+  {
+    name: "Travel Logs",
+    layout: "/admin",
+    path: "/travellogs",
+    icon: <Icon as={MdBook} width='20px' height='20px' color='inherit' />,
     component: Overview,
   },
   {
@@ -33,14 +51,14 @@ const routes = [
     name: "Profile Settings",
     layout: "/admin",
     path: "/profile-settings",
-    icon: <Icon as={MdSettings} width='20px' height='20px' color='inherit' />,
-    component: AccountSettings,
+    icon: <Icon as={ FaUserEdit } width='20px' height='20px' color='inherit' />,
+    component: ProfileSettings,
   },
   {
     name: "Account Settings",
     layout: "/admin",
     path: "/account-settings",
-    icon: <Icon as={MdSettings} width='20px' height='20px' color='inherit' />,
+    icon: <Icon as={ MdManageAccounts } width='20px' height='20px' color='inherit' />,
     component: AccountSettings,
   }
 ];
--- client/src/views/admin/settings/account/index.jsx
+++ client/src/views/admin/settings/account/index.jsx
@@ -1,15 +1,15 @@
 // Chakra imports
 import { Box, SimpleGrid } from "@chakra-ui/react";
+
 // Assets
 import banner from "assets/img/auth/banner.png";
 import profile from "assets/img/crm/vbz.png";
 import React from "react";
+
 // Custom components
 import Info from "views/admin/settings/account/components/Info";
 import Password from "views/admin/settings/account/components/Password";
 import Profile from "views/admin/settings/account/components/Profile";
-import Socials from "views/admin/settings/account/components/Socials";
-import Upload from "views/admin/settings/account/components/Upload";
 import Delete from "views/admin/settings/account/components/Delete"
 
 export default function Settings() {
@@ -18,14 +18,12 @@ export default function Settings() {
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <SimpleGrid
         mb='20px'
-        columns={{ base: 1, sm: 1, md: 3, lg: 3, xl: 3 }}
+        columns={{ base: 1, sm: 1, md: 2, lg: 2, xl: 2 }}
         spacing={{ base: "20px", xl: "20px" }}>
         {/* Components */}
         <Profile avatar={profile} banner={banner} />
         <Info />
         <Password />
-        <Upload />
-        <Socials />
         <Delete />
       </SimpleGrid>
     </Box>
